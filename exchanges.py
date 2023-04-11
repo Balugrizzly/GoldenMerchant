43d4b04edfee35094dbe8b2217a26a79dee104ab
Balu <baluproton+gitlab@protonmail.com>
2023-04-12 00:02:58 +0200

    init exchanges

--- exchanges.py
+++ exchanges.py
@@ -1,5 +1,6 @@
 import ccxt
 import os
+from typing import List, Dict, Any
 
 exchanges = {
     "kucoin": ccxt.kucoin(
@@ -16,3 +17,30 @@ exchanges = {
         }
     ),
 }
+
+
+def initialize_exchanges(exchange_ids: List[str]) -> Dict[str, Any]:
+
+    raise NotImplementedError
+    # Fails on Kucoin because password is required
+    # May initializes exchanges that only have public methods availabe and therefore can have unexpected behavior
+
+    exchanges = {}
+
+    for exchange_id in exchange_ids:
+
+        api_key = os.environ.get(f"{exchange_id.upper()}_API_KEY")
+        secret_key = os.environ.get(f"{exchange_id.upper()}_SECRET_KEY")
+
+        try:
+            exchange = (
+                getattr(ccxt, exchange_id)({"apiKey": api_key, "secret": secret_key})
+                if api_key and secret_key
+                else getattr(ccxt, exchange_id)()
+            )
+            exchanges[exchange_id] = exchange
+
+        except Exception as e:
+            print(f"Couldn't initialize exchange {exchange_id}: {e}")
+
+    return exchanges
